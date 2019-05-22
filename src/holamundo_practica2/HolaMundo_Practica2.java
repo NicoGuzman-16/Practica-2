152d0a474db24d90a554a4aa1e6d89850c28f107
nicow <nicow@Nicols>
2019-05-22 15:47:28 -0500

    práctica 2 - Aplicaciones Distribuidas * Nicolás Guzmán * David Cedeño

---
+++ src/holamundo_practica2/HolaMundo_Practica2.java
@@ -0,0 +1,11 @@
+package holamundo_practica2;
+
+public class HolaMundo_Practica2 {
+    public static void main(String[] args) {
+        System.out.println("********************");
+        System.out.println("     HOLA MUNDO     ");
+        System.out.println(" >> Nicolás Guzmán ");
+        System.out.println(" >> David Cedeño ");
+        System.out.println("********************");
+    }    
+}
